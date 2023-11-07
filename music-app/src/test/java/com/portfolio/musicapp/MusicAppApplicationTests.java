199d2d9c397c103d2ce62f94d28fcfa59ef92734
AjjRibera <110746567+GashaKasha@users.noreply.github.com>
2023-11-06 20:16:09 -0700

    Initial commit

---
+++ music-app/src/test/java/com/portfolio/musicapp/MusicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.musicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
